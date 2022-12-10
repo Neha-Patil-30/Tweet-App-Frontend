fd2babc0e2665e7858a532bcad5c05c52666564a
unknown <cogjava81@iiht.tech>
2022-12-10 19:29:09 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+//  ApiUrl: 'http://13.127.201.229:8090/api/v1.0/tweets',
+  ApiUrl: 'http://15.206.100.172:8090/api/v1.0/tweets',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+
+export const environment = {
+  production: false,
+//  ApiUrl: 'http://13.127.201.229:8090/api/v1.0/tweets'
+  ApiUrl: 'http://15.206.100.172:8090/api/v1.0/tweets'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
